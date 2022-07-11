4e77e45379791a363a50bc40ea68784259ff3893
Rumidich <rumidich@gmail.com>
2022-07-12 01:31:43 +0600

    developed crud on state manager added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import ProductsContextProvider from "./contexts/productsContext";
+import Routing from "./Routing";
 
 const App = () => {
-  return <div>Brand Shoes Online Store</div>;
+  return (
+    <ProductsContextProvider>
+      <Routing />
+    </ProductsContextProvider>
+  );
 };
 
 export default App;
